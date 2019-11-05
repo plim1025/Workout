851a52c3843c7e821a0764b169d43c54b1926574
plim1 <plim1025@gmail.com>
2019-11-05 12:24:53 -0800

    Revert "Commit 25"
    This reverts commit 70e8bdf2

--- app/src/main/java/com/example/android/workout/MainActivity.java
+++ app/src/main/java/com/example/android/workout/MainActivity.java
@@ -1,186 +1,88 @@
 package com.example.android.workout;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.text.DateFormat;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Calendar;
 
-public class WorkoutsActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
-    private static final String ACTIVITY_NAME = WorkoutsActivity.class.getSimpleName();
+    private static final String ACTIVITY_NAME = MainActivity.class.getSimpleName();
     private static final String TAG = ACTIVITY_NAME;
     private FragmentTransaction fragmentTransaction;
-    private ArrayList<Exercise> exercises_to_send = new ArrayList<>();
-    private Bundle bundle = new Bundle();
-
-    private Calendar CALENDAR;
-    private String DATE;
-    private TextView DATEVIEW;
-    private RecyclerView mRecyclerView;
-    private WorkoutsRecyclerViewAdapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private ArrayList<Exercise> exercise = DataHolder.getInstance().exercises;
-    private boolean buildRecycler = false;
+    private ArrayList<Exercise> exercises_to_send = DataHolder.getInstance().exercises;
+    private Fragment fragment;
+    private Fragment initial_fragment;
+    Bundle bundle = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.workouts_activity);
+        setContentView(R.layout.main_activity);
 
         // Set up toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Receive workout arraylist from addExerciseActivity
-        /*Bundle bundle = this.getArguments();
-        if (bundle != null) {
-            buildRecycler = true;
-            ArrayList<Exercise> added_exercises = bundle.getParcelableArrayList("exercises");
-            if(added_exercises!=null) {
-                exercise.addAll(added_exercises);
-            }
-        }
-
-         */
-        if (buildRecycler) {
-            buildRecyclerView();
-        }
-
-        // Set Date
-        DATEVIEW = findViewById(R.id.date);
-        CALENDAR = Calendar.getInstance();
-        DATE = DateFormat.getDateInstance(DateFormat.LONG).format(CALENDAR.getTime());
-        DATEVIEW.setText(DATE);
-
-        // When swipe right/left date changes accordingly
-        View workout_tab = findViewById(R.id.frame);
-        workout_tab.setOnTouchListener(new OnSwipeTouchListener(this) {
-            public void onSwipeRight() {
-                changeDate(-1);
-            }
-            public void onSwipeLeft() {
-                changeDate(1);
-            }
-        });
-
-        // Make arrow buttons change date
-        final ImageButton leftArrow = findViewById(R.id.left_arrow);
-        final ImageButton rightArrow = findViewById(R.id.right_arrow);
-        leftArrow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeDate(-1);
-            }
-        });
-        rightArrow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeDate(1);
-            }
-        });
-        leftArrow.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                changeDate(-7);
-                return true;
-            }
-        });
-        rightArrow.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                changeDate(7);
-                return true;
-            }
-        });
-
-        // Set "Add Exercise" FAB to exercises and switch exercises icon color to blue
-        FloatingActionButton fab1 = findViewById(R.id.workout_fab_1);
-        fab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), AddExerciseActivity.class));
-            }
-        });
-
-        // Set "Copy Workout" FAB to calendar
-        FloatingActionButton fab2 = findViewById(R.id.workout_fab_2);
-        fab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), com.example.android.workout.CalendarActivity.class));
-            }
-        });
-
-
-        // Set "Add Rest" to add rest
-        FloatingActionButton fab3 = findViewById(R.id.workout_fab_3);
-        fab3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TimerDialog timerDialog = new TimerDialog();
-                timerDialog.show(getSupportFragmentManager(), null);
-            }
-        });
-
         // Creates bottom navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         Menu menu = bottomNavigationView.getMenu();
 
-        /*// Receive parcelable ArrayList from AddExerciseSetsActivity and send to WorkoutsFragment
-        ArrayList<Exercise> exercise = getIntent().getParcelableArrayListExtra("exercise");
+        // Receive parcelable ArrayList from AddExerciseActivity and send to WorkoutsFragment
+        ArrayList<Exercise> exercise = getIntent().getParcelableArrayListExtra("exercises");
         if(exercise!=null) {
             exercises_to_send.addAll(exercise);
             bundle.putParcelableArrayList("exercises", exercises_to_send);
-            workoutFragment.setArguments(bundle);
-        }*/
+        }
 
-        // Set initial fragment to workouts
+        // Set home fragment to workout tab
         MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
+        initial_fragment = new WorkoutsFragment();
+        initial_fragment.setArguments(bundle);
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frame, initial_fragment, "0");
+        fragmentTransaction.addToBackStack("Add " + initial_fragment.toString());
+        fragmentTransaction.commit();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            Fragment fragment;
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.nav_workout:
+                        fragment = initial_fragment;
                         break;
                     case R.id.nav_exercises:
                         fragment = new ExercisesFragment();
-                        fragmentTransaction = fragmentManager.beginTransaction();
-                        fragmentTransaction.replace(R.id.frame, fragment, "0");
                         break;
                     case R.id.nav_progress:
                         fragment = new ProgressFragment();
-                        fragmentTransaction = fragmentManager.beginTransaction();
-                        fragmentTransaction.replace(R.id.frame, fragment, "0");
                         break;
                     default:
                         throw new IllegalStateException("Unexpected value: " + menuItem.getItemId());
                 }
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame, fragment, "0");
+
                 // Need to title it so that fragment tier 2 can return to tier 1 after pressing android back button
                 fragmentTransaction.addToBackStack("Add main");
                 fragmentTransaction.commit();
@@ -200,21 +102,6 @@ public class WorkoutsActivity extends AppCompatActivity {
                 Log.i(TAG, backstackEntryMessage.toString());
             }
         });
-    }
-
-    private void changeDate(int i) {
-        CALENDAR.add(Calendar.DATE, i);
-        DATE = DateFormat.getDateInstance(DateFormat.LONG).format(CALENDAR.getTime());
-        DATEVIEW.setText(DATE);
-    }
-
-    private void buildRecyclerView() {
-        mRecyclerView = findViewById(R.id.workout_recycler_view);
-        mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        mAdapter = new WorkoutsRecyclerViewAdapter(exercise);
-
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
     }
 
     @Override
